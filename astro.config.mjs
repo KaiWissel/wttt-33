0be21b35da1d2c307d6f24a79122e228aaf53471
KaiWissel <kai.wpkt@gmail.com>
2023-02-25 20:31:20 +0100

    added user icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-// import node from "@astrojs/node";
+import node from "@astrojs/node";
 import netlify from "@astrojs/netlify/edge-functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  // adapter: node({
-  //   mode: "standalone"
-  // })
-  adapter: netlify(),
+  adapter: node({
+    mode: "standalone",
+  }),
+  // adapter: netlify(),
 });
