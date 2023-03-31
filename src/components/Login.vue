ba35b01b463bd1b7cd7d042f2a88c8c22e58c36c
KaiWissel <kai.wpkt@gmail.com>
2023-03-31 18:02:05 +0200

    first interactive login

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <select v-model="selected">
+      <option v-for="option in loginOptions" :value="option">
+        {{ option }}
+      </option>
+    </select>
+    <input type="password" placeholder="Passwort" v-model="password" />
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const loginOptions = ["Viewer", "Admin"];
+
+const selected = ref(loginOptions[0]);
+const password = ref();
+
+function login() {
+  console.log("Login: ", selected.value, password.value);
+}
+</script>
+
+<style scoped></style>
