e2d44a489fefceb6c60c1cb82db43090f5647db0
KaiWissel <kai.wpkt@gmail.com>
2023-04-22 13:00:02 +0200

    added types to env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly WTTT_33_JWT_PRIVATE_KEY: string;
+  readonly WTTT_33_JWT_TIME: number;
+  readonly PUBLIC_API_URL: string;
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
