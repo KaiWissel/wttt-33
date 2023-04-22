c9daef7df1d87b21d2eedebea7184db414e39d5a
KaiWissel <kai.wpkt@gmail.com>
2023-04-22 13:24:50 +0200

    use as express server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from "astro/config";
-import { prisma } from "./src/plugins/prisma-client";
-
-//
 import node from "@astrojs/node";
 // import netlify from "@astrojs/netlify/edge-functions";
 
@@ -12,9 +9,9 @@ import vue from "@astrojs/vue";
 export default defineConfig({
   output: "server",
   adapter: node({
-    mode: "standalone",
+    mode: "middleware",
   }),
   server: { port: 1234, host: true },
   //   // adapter: netlify(),
-  integrations: [vue(), prisma],
+  integrations: [vue()],
 });
---
+++ server.mjs
@@ -0,0 +1,10 @@
+import express from "express";
+import { handler as ssrHandler } from "./dist/server/entry.mjs";
+import morgan from "morgan";
+
+const app = express();
+app.use(express.static("dist/client/"));
+app.use(morgan("combined"));
+app.use(ssrHandler);
+
+app.listen(54321);
