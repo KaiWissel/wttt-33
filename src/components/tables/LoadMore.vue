388a7593e79adf44b832b482fdf5261c0d168e2e
KaiWissel <kai.wpkt@gmail.com>
2023-10-22 16:08:38 +0200

    update table if button becomes visible

--- src/components/tables/LoadMore.vue
+++ src/components/tables/LoadMore.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
-defineProps({
+import { ref, watchEffect } from "vue";
+import { useElementVisibility } from "@vueuse/core";
+
+const props = defineProps({
   disableLoad: Boolean,
+});
+
+const target = ref(null);
+const targetIsVisible = useElementVisibility(target);
+
+const emit = defineEmits(["loadMore"]);
+
+watchEffect(() => {
+  console.log("Target is visible: ", targetIsVisible.value);
+  if (targetIsVisible.value && !props.disableLoad) {
+    // TODO: 22/10/23 Update will currently reload whole page.
+    // This is not such a nice UX.
+    // Therefore only load new data if user forces this.
+    // emit("loadMore");
+  }
 });
 </script>
 
 <template>
-  <button @click="$emit('loadMore')" :disabled="disableLoad">
+  <button @click="$emit('loadMore')" ref="target" :disabled="disableLoad">
     Lade mehr Einträge
   </button>
 </template>
