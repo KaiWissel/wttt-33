b4068fde9e1289551f06ed820335cfa307797cdf
KaiWissel <kai.wpkt@gmail.com>
2024-04-07 22:22:36 +0200

    more doc

--- README.md
+++ README.md
@@ -23,6 +23,7 @@ All commands are run from the root of the project, from a terminal:
 ## Build and setup WTTT-33 server
 
 - `cp .env.sample .env`     Create an .env file by copying the .env.sample
+- Adjust .env > PUBLIC_API_URL to the URL of the actual host machine
 - `docker compose build`    Builds the application image                                               
 - `docker compose up -d`    Starts the whole application environment 
 - `npx prisma generate`     Generates the correct data source client code   
--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -3,7 +3,7 @@ import users from "../test/data/users.json" assert { type: "json" };
 import bookings from "../test/data/bookings.json" assert { type: "json" };
 import courses from "../test/data/courses.json" assert { type: "json" };
 import courseTypes from "../test/data/courseTypes.json" assert { type: "json" };
-import authentication from "../test/data/authentication.json" assert { type: "json" };
+import authentssication from "../test/data/authentication.json" assert { type: "json" };
 
 const prisma = new PrismaClient();
 
@@ -14,7 +14,7 @@ async function main() {
   await prisma.courseType.deleteMany({});
   await prisma.authentication.deleteMany({});
 
-  const resCt = await prisma.courseType.createMany({
+  const resCt = await prisma.courseType.crseateMany({
     data: courseTypes,
   });
 
