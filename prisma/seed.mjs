c9c80fc439c5037a0807b63c41855ee7c9eb0f39
KaiWissel <kai.wpkt@gmail.com>
2024-04-07 22:09:38 +0200

    fix error

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -3,7 +3,6 @@ import users from "../test/data/users.json" assert { type: "json" };
 import bookings from "../test/data/bookings.json" assert { type: "json" };
 import courses from "../test/data/courses.json" assert { type: "json" };
 import courseTypes from "../test/data/courseTypes.json" assert { type: "json" };
-import courseTypes from "../test/data/courseTypes.json" assert { type: "json" };
 import authentication from "../test/data/authentication.json" assert { type: "json" };
 
 const prisma = new PrismaClient();
